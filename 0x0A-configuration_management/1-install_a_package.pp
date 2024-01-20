4c7918322a7b726d9a74307b7010a4520a0b828a
loay ashraf <117324979+loay160@users.noreply.github.com>
2024-01-20 00:39:23 +0000

    ad

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,8 @@
-# install puppet-lint using Puppet
+#!/usr/bin/pup
 
-package { 'puppet-lint':
-  ensure   => '2.1.1',
-  provider => 'gem',
-}
+# Install an especific version of flask (2.1.0)
+
+package {'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3'
+	}
