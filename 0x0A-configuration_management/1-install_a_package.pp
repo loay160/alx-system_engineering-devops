32a7f855971c5c5611b23b006666288320be0fca
loay ashraf <117324979+loay160@users.noreply.github.com>
2024-01-20 03:12:57 +0200

    Update 1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -2,6 +2,7 @@
 # installing a specific version of flask 2.1.0
 
 package { 'flask':
-  ensure   => 'created',
+  ensure   => '2.1.0',
   provider => 'pip3',
+  require  => Class['python::devel'],
 }
